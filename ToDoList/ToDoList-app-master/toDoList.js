3035c3284ce7036d32641b86777ed6cf57912b62
kalpana5meena <44996470+kalpana5meena@users.noreply.github.com>
2020-08-09 20:53:03 +0530

    Add files via upload

---
+++ ToDoList/ToDoList-app-master/toDoList.js
@@ -0,0 +1,18 @@
+$("ul").on("click","li",function(){
+    $(this).toggleClass("completed")
+});
+
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(200,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress",function(event){
+    if(event.which === 13){
+        var todo = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i> </span>" + todo + "</li>");
+        }
+});
